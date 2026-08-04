8fb76d78f55cd91f5f08a48fa5ea18d21012c896
manuv-dev <komi-emmanuel.nyadzi@ism.edu.sn>
2026-08-04 09:30:45 +0000

    config: prepare for Render deployment

--- src/main/java/com/kikitraiteur/api_kikitraiteur/Client/service/ClientService.java
+++ src/main/java/com/kikitraiteur/api_kikitraiteur/Client/service/ClientService.java
@@ -1,8 +1,9 @@
 package com.kikitraiteur.api_kikitraiteur.Client.service;
 
+import java.util.List;
+
 import com.kikitraiteur.api_kikitraiteur.Client.dto.ClientDto;
 import com.kikitraiteur.api_kikitraiteur.Client.model.Client;
-import java.util.List;
 
 public interface ClientService {
     Client getOrCreateClient(String email, String name, String phone, String type, String organization);
